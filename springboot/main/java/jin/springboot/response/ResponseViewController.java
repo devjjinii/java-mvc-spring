f2d2cafb5ac008215b61a2d8f843dc03922c753f
YuJin Choi <53853730+devjjinii@users.noreply.github.com>
2021-06-28 15:08:54 +0900

    feat: HTTP 요청 파라미터 URL 추가

---
+++ springboot/main/java/jin/springboot/response/ResponseViewController.java
@@ -0,0 +1,24 @@
+package jin.springboot.response;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ResponseViewController {
+
+    @RequestMapping("/basic/hello-form.html")
+    public ModelAndView responseViewV1() {
+        ModelAndView mav = new ModelAndView("response/hello")
+                .addObject("data", "hello~!");
+
+        return mav;
+    }
+
+    @RequestMapping("/response-view-v2")
+    public String responseViewV2(Model model) {
+        model.addAttribute("data", "hello!");
+        return "response/hello";
+    }
+}
